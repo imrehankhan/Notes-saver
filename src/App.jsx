2c7d3f4b34515ba9395c302f29cb6950a6036a8a
Rehan khan <mdrk1313@gmail.com>
2025-01-28 20:13:37 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
-import Paste from './components/paste'
+import Paste from './components/Paste'
 import ViewPaste from './components/ViewPaste'
 
 function App() {
